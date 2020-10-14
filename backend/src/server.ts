fab84f3b6010831091eb020eacb838c7ee127587
Henrique Tinem <henrique714tinem@gmail.com>
2020-10-14 12:08:58 -0300

    day 2 nlw

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import './database/connection';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({message: 'Hello World'})
+});
+
+app.listen(3333);
