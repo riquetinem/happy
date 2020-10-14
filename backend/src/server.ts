8ec4fb4455bda3de8ff8ff8aeeefef35c7f43a81
Henrique Tinem <henrique714tinem@gmail.com>
2020-10-14 12:09:51 -0300

    day 2 nwl

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
 
 import './database/connection';
 
-const app = express();
-app.use(express.json());
+import routes from './routes';
+import errorHandler from './errors/handler';
 
-app.get('/', (req, res) => {
-  return res.json({message: 'Hello World'})
-});
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
